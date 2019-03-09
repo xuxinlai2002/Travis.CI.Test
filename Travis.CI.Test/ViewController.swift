9215912a92cce6328c8c90444a881cf77566bd79
xxl <xxl@local.com>
2019-03-09 18:26:42 +0800

    add the project

---
+++ Travis.CI.Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travis.CI.Test
+//
+//  Created by xu xinlai on 2019/3/9.
+//  Copyright © 2019年 xu xinlai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
